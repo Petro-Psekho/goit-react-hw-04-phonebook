6cf42796b9c947ffb717d8e7af99dc776395a831
Petro Psekho <psekho.pp@gmail.com>
2023-03-02 12:39:13 +0200

    react-hook-form in progress

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,75 +1,125 @@
-import { Formik, ErrorMessage } from 'formik';
-import { object, string } from 'yup';
-import {
-  ErrMessage,
-  FormWrap,
-  FormInput,
-  InputName,
-  SubmitBtn,
-} from 'components/ContactForm/ContactForm.styled';
-
-const namePattern =
-  /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
-
-const numberPattern =
-  /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
-
-const schema = object().shape({
-  name: string()
-    .max(20)
-    .matches(namePattern, {
-      message:
-        "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan.",
-    })
-    .required(),
-  number: string()
-    .min(3)
-    .matches(numberPattern, {
-      message:
-        'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +.',
-    })
-    .required(),
-});
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export const ContactForm = ({ onSubmit }) => {
-  const initialValues = {
-    name: '',
-    number: '',
-  };
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm({
+    defaultValuers: {
+      name: '',
+      number: '',
+    },
+  });
+  // const onSubmit = data => {
+  //   console.log(data);
+  // };
 
-  const handleSubmit = (values, { resetForm }) => {
-    onSubmit(values);
+  console.log(errors);
 
-    resetForm();
-  };
+  // console.log(watch());
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={schema}
-      onSubmit={handleSubmit}
-    >
-      <FormWrap>
-        <InputName>
-          Name
-          <FormInput autoComplete="off" type="text" name="name" />
-        </InputName>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input
+        autoComplete="off"
+        {...register('name', {
+          required: 'name is a required field',
+          minLength: { value: 3, message: 'min lenght is 3' },
+        })}
+        placeholder="Enter Name"
+      />
+      <p>{errors.name?.message}</p>
 
-        <ErrMessage>
-          <ErrorMessage name="name" />
-        </ErrMessage>
+      <label>Number</label>
+      <input
+        autoComplete="off"
+        {...register('number', { required: 'number is a required field' })}
+        placeholder="Enter Number"
+      />
+      <p>{errors.number?.message}</p>
 
-        <InputName>
-          Number
-          <FormInput autoComplete="off" type="tel" name="number" />
-        </InputName>
-
-        <ErrMessage>
-          <ErrorMessage name="number" component="div" />
-        </ErrMessage>
-
-        <SubmitBtn type="submit">Add contact</SubmitBtn>
-      </FormWrap>
-    </Formik>
+      {errors.exampleRequired && <p>This field is required</p>}
+      <input type="submit" />
+    </form>
   );
 };
+
+// import { Formik, ErrorMessage } from 'formik';
+// import { object, string } from 'yup';
+// import {
+//   ErrMessage,
+//   FormWrap,
+//   FormInput,
+//   InputName,
+//   SubmitBtn,
+// } from 'components/ContactForm/ContactForm.styled';
+
+// const namePattern =
+//   /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+
+// const numberPattern =
+//   /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
+
+// const schema = object().shape({
+//   name: string()
+//     .max(20)
+//     .matches(namePattern, {
+//       message:
+//         "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan.",
+//     })
+//     .required(),
+//   number: string()
+//     .min(3)
+//     .matches(numberPattern, {
+//       message:
+//         'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +.',
+//     })
+//     .required(),
+// });
+
+// export const ContactForm = ({ onSubmit }) => {
+//   const initialValues = {
+//     name: '',
+//     number: '',
+//   };
+
+//   const handleSubmit = (values, { resetForm }) => {
+//     onSubmit(values);
+
+//     resetForm();
+//   };
+
+//   return (
+//     <Formik
+//       initialValues={initialValues}
+//       validationSchema={schema}
+//       onSubmit={handleSubmit}
+//     >
+//       <FormWrap>
+//         <InputName>
+//           Name
+//           <FormInput autoComplete="off" type="text" name="name" />
+//         </InputName>
+
+//         <ErrMessage>
+//           <ErrorMessage name="name" />
+//         </ErrMessage>
+
+//         <InputName>
+//           Number
+//           <FormInput autoComplete="off" type="tel" name="number" />
+//         </InputName>
+
+//         <ErrMessage>
+//           <ErrorMessage name="number" component="div" />
+//         </ErrMessage>
+
+//         <SubmitBtn type="submit">Add contact</SubmitBtn>
+//       </FormWrap>
+//     </Formik>
+//   );
+// };
